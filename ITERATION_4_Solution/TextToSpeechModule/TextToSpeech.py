d3459da381b7f3423866c928efdaebdf8e554c1f
Asfand Ali Irfan <i191656@nu.edu.pk>
2023-05-06 18:14:31 +0500

    New TTS Module

---
+++ ITERATION_4_Solution/TextToSpeechModule/TextToSpeech.py
@@ -0,0 +1,98 @@
+import os
+import torch
+import torchaudio
+
+class TextToSpeech:
+    def __init__(self):
+        model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                                             model='silero_tts',
+                                             language='indic',
+                                             speaker='v3_indic')
+        self.model = model
+        self.urdu_to_roman_urdu_dict = {
+                               'ا': 'a',
+                               'آ': 'a',
+                               'أ': 'a',
+                               'ب': 'b',
+                               'پ': 'p',
+                               'ت': 't',
+                               'ٹ': 't',
+                               'ث': 's',
+                               'ج': 'j',
+                               'چ': 'ch',
+                               'ح': 'h',
+                               'خ': 'kh',
+                               'د': 'd',
+                               'ڈ': 'd',
+                               'ذ': 'dh',
+                               'ر': 'r',
+                               'ڑ': 'r',
+                               'ز': 'z',
+                               'ژ': 'zh',
+                               'س': 's',
+                               'ش': 'sh',
+                               'ص': 's',
+                               'ض': 'z',
+                               'ط': 't',
+                               'ظ': 'z',
+                               'ع': 'a',
+                               'غ': 'gh',
+                               'ف': 'f',
+                               'ق': 'q',
+                               'ک': 'k',
+                               'گ': 'g',
+                               'ل': 'l',
+                               'م': 'm',
+                               'ن': 'n',
+                               'ں': 'n',
+                               'و': 'o',
+                               'ﺅ': 'o',
+                               'ہ': 'h',
+                               'ۂ': 'h',
+                               'ۃ': 'h',
+                               'ھ': 'h',
+                               'ء': 'h',
+                               'ی': 'i',
+                               'ئ': 'i',
+                               'ے': 'e',
+                               'ۓ': 'e',
+                               '۰': '0',
+                               '۱': '1',
+                               '۲': '2',
+                               '۳': '3',
+                               '۴': '4',
+                               '۵': '5',
+                               '۶': '6',
+                               '۷': '7',
+                               '۸': '8',
+                               '۹': '9',
+                               '۔': '.',
+                               '؟': '?'
+                          }
+        
+    def urdu2roman(self,
+                   text: str) -> str:
+        return ''.join([self.urdu_to_roman_urdu_dict[char] if char in self.urdu_to_roman_urdu_dict else char for char in text])
+
+    def predict(self,
+                text: str):
+        if not text.isascii():
+            text = self.urdu2roman(text)
+        audio = self.model.apply_tts(text,
+                                     speaker='hindi_female',
+                                     sample_rate=48000)
+        return audio
+    
+    def predict_and_save(self,
+                         text: str,
+                         save_path: str = "",
+                         file_name: str = "tts_audio") -> None:
+        if not text.isascii():
+            text = self.urdu2roman(text)
+        audio = self.model.apply_tts(text,
+                                     speaker='hindi_female',
+                                     sample_rate=48000)
+        file_name = file_name.replace(" ", "_")
+        file_name = file_name + '.wav'
+        path = os.path.join(save_path, file_name)
+        torchaudio.save(path, audio.view(1, -1), 48000)
